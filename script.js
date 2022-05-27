4fac408ff68cec796681bcdb24256bc79cadd8b9
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-05-27 14:30:24 -0300

    Requisitos 7 a 11

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
-const buttonAdicionar = document.getElementsByTagName('button')[0];
+const buttonApagaTarefa = document.getElementById('remover-finalizados');
+const buttonAdicionar = document.getElementById('criar-tarefa');
+const buttonApagar = document.getElementById('apaga-tudo');
 const input = document.getElementsByTagName('input')[0];
 const ol = document.getElementsByTagName('ol')[0];
+const tarefa = document.getElementsByClassName('tarefa');
+const completa = document.getElementsByClassName('completed');
 
 function adicionaTarefa() {
   const li = document.createElement('li');
   li.innerText = input.value;
+  li.className = 'tarefa';
+  li.addEventListener('click', adicionaClasse);
+  li.addEventListener('dblclick', tarefaCompleta);
   ol.appendChild(li);
   input.value = '';
 }
 
-buttonAdicionar.addEventListener('click', adicionaTarefa);
+buttonAdicionar.addEventListener('click', function() {
+  adicionaTarefa();
+});
+
+function mudaCorItem() {
+  document.querySelector('.selecionada').style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function adicionaClasse(event) {
+  const selecionada = document.querySelector('.selecionada');
+  if (selecionada === null) {
+    event.target.classList.add('selecionada');
+    mudaCorItem();
+  }
+  selecionada.style.backgroundColor = 'white';
+  selecionada.classList.remove('selecionada');
+  event.target.classList.add('selecionada');
+  mudaCorItem();
+}
+
+function tarefaCompleta(event) {
+  if (event.target.className === 'tarefa selecionada') {
+  event.target.style.textDecoration = 'line-through solid black';
+  event.target.className = 'completed';
+  event.target.style.backgroundColor = 'white';
+} else { event.target.style.textDecoration = 'none';
+  event.target.className = 'tarefa selecionada';
+}
+}
+
+function apagaLista() {
+  while (tarefa[0] !== undefined) {
+    tarefa[0].remove();
+  }
+}
+buttonApagar.addEventListener('click', apagaLista);
+
+function apagaTarefasCompleta() {
+  while (completa[0] !== undefined) {
+    completa[0].remove();
+  }
+}
+buttonApagaTarefa.addEventListener('click', apagaTarefasCompleta);
